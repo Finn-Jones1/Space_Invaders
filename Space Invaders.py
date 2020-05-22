9e2bfbe031472db556765185d9b1fd5f477056c1
BuildTools <unconfigured@null.spigotmc.org>
2020-05-22 17:08:16 +1000

    Enemies and collision detection implemented

--- Space Invaders.py
+++ Space Invaders.py
@@ -1,61 +1,79 @@
-import pygame, sys
+import pygame, sys, os, random
 
 clock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
-
+FlexyPath = os.path.dirname(os.path.abspath(__file__))
+missileY = 0
 playerSpeed = 15
 display_width = 1000
 display_height = 700
+Shot = False
+
+
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
-ls = [pygame.image.load("/Users/finnjones/IST/E1.png"), pygame.image.load("/Users/finnjones/IST/E2.png"), pygame.image.load("/Users/finnjones/IST/E3.png")]
-pygame.display.set_caption('A bit Racey')
+# ls = [pygame.image.load(FlexyPath+"/E1.png"), pygame.image.load(FlexyPath+"/E2.png"), pygame.image.load(FlexyPath+"/E3.png")]
+pygame.display.set_caption('Space Invaders')
 
 black = (0,0,0)
 white = (255,255,255)
+enemySpeed = 15
+enemySpacing = 100
+stop = False
+enemyImg = []
+Ex = []
+Ey = []
+eChangeX = []
+eChangeY = []
+numEnemies = 6
+for i in range(numEnemies):
+    enemyImg.append(pygame.image.load(FlexyPath+"/E1.png"))
+    eChangeX.append(enemySpeed)
+    eChangeY.append(10)
+    Ex.append(100 + enemySpacing * i)
+    Ey.append(100)
+EnemyW = 55
+EnemyL = 78
 
-
-crashed = False
-playerImg = pygame.image.load('/Users/finnjones/IST/Player.png')
 x =  (display_width * 0.45)
 y = (display_height * 0.8)
 x_change = 0
 
-missileImg = pygame.image.load('/Users/finnjones/IST/Missile.png')
-
-missileX = x + 20
-missileY = y
+# missileX = x + 20
+# missileY = y
 missileY_change = -30
 missileFire = "ready"
-walkCount = 0
+# walkCount = 0
 
+
+hitList = []
 
 def player(x,y):
-    global walkCount
-    if walkCount + 1 >= 3:
-        walkCount = 0
-    gameDisplay.blit(ls[walkCount//3], (x,y))  # We integer divide walkCounr by 3 to ensure each
+    gameDisplay.blit(pygame.image.load(FlexyPath+"/Player.png"), (x,y))  # We integer divide walkCounr by 3 to ensure each
 
+def enemy(x,y,i):
+    gameDisplay.blit((enemyImg[i]), (x,y))
 
 def fireMisslie(x,y):
-    gameDisplay.blit(missileImg, (x,y))
+    gameDisplay.blit(pygame.image.load(FlexyPath+'/Missile.png'), (x,y))
 
 
      
 
-
-while not crashed:
+while not stop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            crashed = True
+            stop = True
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 x_change -= playerSpeed
             elif event.key == pygame.K_d:
                 x_change += playerSpeed
+            elif event.key == pygame.K_ESCAPE:
+                stop = True
             elif event.key == pygame.K_SPACE:
                 if missileFire == "ready":
                     missileX = x + 20
@@ -71,19 +89,47 @@ while not crashed:
 
     gameDisplay.fill(black)
 
+
+
+
+
     if missileY <=0:
         missileFire = "ready"
 
     if missileFire == "fire":
         fireMisslie(missileX,missileY)
         missileY += missileY_change
+        for i in range(numEnemies):
+            if Ey[i] > missileY:
+                print("y crossover")
+                if Ex[i] < missileX and Ex[i] > missileX - 59:
+                    hitList.append(str(i))
+                    Shot = True
+                    missileFire = "ready"
+                    print("x crossover")
+
 
     x += x_change
 
+    # if Shot == False:
+    for i in range(numEnemies):
+        if str(i) not in str(hitList) :
+            Ex[i] += eChangeX[i] 
+            enemy(Ex[i],Ey[i], i)
+
+        if Ex[i] > display_width - 65:
+            eChangeX[i] = -enemySpeed
+        
+        if Ex[i] < 0 - 20:
+            eChangeX[i] = enemySpeed
+
+
     player(x,y)
 
+
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
 
 pygame.quit()
 quit()
