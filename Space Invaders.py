a174da9dac27b5a4883e0909e22e3a5e5e4476c1
Finn-Jones1 <finn.jones@student.tsc.nsw.edu.au>
2020-06-26 18:15:22 +1000

    Game Done

--- Space Invaders.py
+++ Space Invaders.py
@@ -1,6 +1,5 @@
 # Importing Dependencies
-import pygame, sys, os, random, time, datetime
-from pygame.locals import *
+import pygame, os, random, time, datetime
 
 # Starting pygame's clock 
 clock = pygame.time.Clock()
@@ -40,7 +39,6 @@ Scontents = contents.split()
 highestLevel = Scontents[0]
 
 highestScore = Scontents[1]
-
 
 # variables for gameplay
 enemySpacing = 50
@@ -86,7 +84,6 @@ for i in range(numEnemies):
     eMY.append(100)
     
 
-
 # Defining Sound Effects
 hitSound = pygame.mixer.Sound(FlexyPath+"/sound/Hit_Hurt.wav")
 click = pygame.mixer.Sound(FlexyPath+"/sound/Blip_Select.wav")
@@ -113,7 +110,7 @@ def save():
 
     f.write(str(highestLevel)+ " " + str(highestScore))
 
-# This function
+# Functions for controling sprites
 def boom(x,y):
     screen.blit(pygame.image.load(FlexyPath+"/sprites/Boom.png"),(x,y))
 
@@ -135,67 +132,81 @@ def fireMisslie(x,y):
 def eFireMisslie(x,y,i):
     screen.blit((eMissile[i]), (x,y))
 
+# Rendering Font with colour and text
 def textRender(text, font , colour):
     textSurface = font.render(text, True, colour)
     return textSurface, textSurface.get_rect()
 
+# This function can be used to put any text on game interface
 def showText(text, fontSize, textloc, colour):
     Font = pygame.font.Font(FlexyPath+'/Quicksand-VariableFont_wght.ttf', fontSize)
     finalText, textLoc = textRender(text, Font , colour)
     screen.blit(finalText, textloc)
 
+# Player shooting enemy hit detection
 def hitDetect(x,y,x1,y1, hitSizeW, hitSizeL):
     if x - hitSizeW < x1 and x + hitSizeW > x1 and y + hitSizeL > y1 and y - hitSizeL < y1:
         return True
     else:
         return False
   
+# Introduction Scene UI ect
 def Intro():
+    # Making Variables global
     global joyConnect
     global powerupSelector
     global selection
+
     joyConnect = "false"
     controllerStatus = "Off"
+
+    # Power-up List
     powerupSelector = ["Double Points", "Cluster Shot", "Super Speed"]
     selection = 0
     stop = False
     
+    # Setting backround image
     screen.blit(background_image, [0, 0])
     
     # Start Game
     pygame.draw.rect(screen,white,(404,500,100,50))
-    # Powerup Switcher Button
+    # Power-up Switcher Button
     pygame.draw.rect(screen,white,(370,400,170,50))
-    # Controler On / Off
+    # Power-up On / Off
     pygame.draw.rect(screen,white,(380,600,150,50))
 
+    # Displaying text
     showText('Start', 30, (420 ,505), black)
     showText('Controller '+ controllerStatus, 20, (396 ,610), black)
     showText('Pew Pew 2D', 50, (100 ,100), white)
     showText('High Score: '+ str(highestScore), 30, (100 ,170), white)
     showText('High Level: '+ str(highestLevel), 30, (100 ,210), white)
-    showText('Powerup Selecter: ', 20, (375 ,405), black)
+    showText('Power-up Selector: ', 20, (375 ,405), black)
     showText(powerupSelector[selection], 15, (402 ,430), black)
 
+    # Starting Pygame event loop
     while not stop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
 
 
+            # Joystick controller status
             if joyConnect == "true":
                 controllerStatus = "On"
             elif joyConnect == "false":
                 controllerStatus = "Off"
 
 
-            
+            # Detecting if mouse is hovering over the button
             if pygame.mouse.get_pos()[0] > 405 and pygame.mouse.get_pos()[0] < 505:
                 if pygame.mouse.get_pos()[1] > 500 and pygame.mouse.get_pos()[1] < 550:
+                    # Mouse hovering detection
                     pygame.draw.rect(screen,lightGrey,(404,500,100,50))
                     showText('Start', 30, (420 ,505), black)
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         click.play()
+                        # Button darkens when pressed
                         pygame.draw.rect(screen,grey,(404,500,100,50))
                         showText('Start', 30, (420 ,505), black)
                     elif event.type == pygame.MOUSEBUTTONUP:
@@ -206,7 +217,7 @@ def Intro():
                 else:
                     pygame.draw.rect(screen,white,(404,500,100,50))
                     showText('Start', 30, (420 ,505), black)
-
+            # Same technique used for the other buttons
 
             if pygame.mouse.get_pos()[0] > 380 and pygame.mouse.get_pos()[0] < 529:
                 if pygame.mouse.get_pos()[1] > 600 and pygame.mouse.get_pos()[1] < 649:
@@ -236,7 +247,7 @@ def Intro():
             if pygame.mouse.get_pos()[0] > 370 and pygame.mouse.get_pos()[0] < 540:
                 if pygame.mouse.get_pos()[1] > 400 and pygame.mouse.get_pos()[1] < 450:
                     pygame.draw.rect(screen,lightGrey,(370,400,170,50))
-                    showText('Powerup Selecter: ', 20, (375 ,405), black)
+                    showText('Power-up Selector: ', 20, (375 ,405), black)
                     showText(powerupSelector[selection], 15, (402 ,430), black)
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         selection = selection + 1
@@ -245,21 +256,23 @@ def Intro():
                         if len(powerupSelector) == selection:
                             selection = 0
                         pygame.draw.rect(screen,grey,(370,400,170,50))
-                        showText('Powerup Selecter: ', 20, (375 ,405), black)
+                        showText('Power-up Selector: ', 20, (375 ,405), black)
                         showText(powerupSelector[selection], 15, (402 ,430), black)
                         
                         print("change")
             
                 else:
                     pygame.draw.rect(screen,white,(370,400,170,50))
-                    showText('Powerup Selecter: ', 20, (375 ,405), black)
+                    showText('Power-up Selector: ', 20, (375 ,405), black)
                     showText(powerupSelector[selection], 15, (402 ,430), black)
 
-
+            # Updating screen with tick speed limiter (Clock tick)
             pygame.display.update()
             clock.tick(120)
 
+# Main Game Loop
 def gameLoop():
+    # Retreving global variables
     global levelCounter
     global enemySpeed
     global numEnemies
@@ -279,17 +292,15 @@ def gameLoop():
     y = (display_height * 0.8)
     axis = ''
     missileY = 0
-    EnemyW = 28
-    EnemyL = 78
     missileX = 0
     missileY = 0
     missileY_change = -30
     missileFire = "ready"
-    
-    connectEndLoop = "false"
-    boundaries = 0
 
+    # Getting all the buttons on joystick
+    joystick_count = pygame.joystick.get_count()
     
+    # Checking for selected Power-up
 
     if powerupSelector[selection] == "Super Speed":
         playerSpeed = 30
@@ -309,6 +320,7 @@ def gameLoop():
 
     x_change = 0
     stop = False
+    # pygame game loop
     while not stop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -323,10 +335,8 @@ def gameLoop():
                     missileFire = "fire"
 
 
-
+            # Overcomplicated key detection most cases don't need this but switching between screens caused issues otherwise
             if event.type == pygame.KEYDOWN:
-                
-
                 if event.key == pygame.K_SPACE:
                     if missileFire == "ready":
                         Shoot.play()
@@ -341,10 +351,6 @@ def gameLoop():
                 elif event.key == pygame.K_d:
                     x_change += playerSpeed
                     movementEprevent = 1
-                    
-                # elif event.key == pygame.K_ESCAPE:
-                #     stop = True
-
 
             if event.type == pygame.KEYUP:
                 
@@ -360,10 +366,8 @@ def gameLoop():
                     else:
                         x_change = 0
                     movementEprevent = 0
-            # else:
-            #     print("ting")
-            #     x_change = 0
 
+        # Preventing player from moving off screen
 
         if x < 10:
             if x_change < 0:
@@ -376,46 +380,55 @@ def gameLoop():
                 movementEprevent = 0
 
 
-
+        # Setting backround image
         screen.blit(background_image, [0, 0])
 
-        joystick_count = pygame.joystick.get_count()
 
-        for i in range(joystick_count):
-            joystick = pygame.joystick.Joystick(i)
-            joystick.init()
-            axis = joystick.get_axis(0)
-
+        # Checking if controller was enabled from the intro screen
         if joyConnect == "true":
-
+            # Checking for joystick updates
+            for i in range(joystick_count):
+                joystick = pygame.joystick.Joystick(i)
+                joystick.init()
+                axis = joystick.get_axis(0)
             if axis > 0.5:
                 x = x + playerSpeed * axis
             elif axis < -0.01:
                 x = x + playerSpeed * axis
 
+        # Checking if missile has moved off screen
         if missileY < -20:
             missileFire = "ready"
 
+        # Checking if missile is in shooting mode
         if missileFire == "fire":
+            # Utilising hit detection function from earlier
             if hitDetect(BossX,BossY, missileX, missileY, 30, 30) is True:
+                # Playing explosion sound
                 Explosion.play()
+                # Explosion sprite
                 boom(BossX, BossY)
+                # Double points power-up?
                 if doublePoints is True:
                     Score = Score + 200
                 else:
                     Score = Score + 100
-                print(Score)
                 BossX = 2000
+                # Resetting missile
                 missileFire = "ready"
             fireMisslie(missileX,missileY)
             missileY += missileY_change
             for i in range(numEnemies):
                 print("y crossover")
+                # Hit detection for main enemies
                 if hitDetect(Ex[i],Ey[i], missileX, missileY, 25, 30) is True:
                     if i not in hitList:
                         Explosion.play()
                         boom(Ex[i], Ey[i])
+                        # Dead enemies will be added to this list
                         hitList.append(i)
+
+                        # Kills nearby enemies
                         if clusterShot is True:
                             if i - 1 >= 0 and i - 1 not in hitList:
                                 boom(Ex[i - 1], Ey[i - 1])
@@ -428,22 +441,23 @@ def gameLoop():
                             Score = Score + 20
                         else:
                             Score = Score + 10
-                        # Shot = True
                         missileFire = "ready"
-                        print("x crossover")
 
 
 
 
+        # Enemy control
         for i in range(numEnemies):
+            # Removing Dead Enemies
             if i not in hitList:
-                
-                
+                # Enemy Movement
                 Ex[i] += eChangeX[i]
                 enemy(Ex[i],Ey[i], i)
 
+                # Randomising and delaying shooting
                 shootDelay = random.randint(0,250)
-
+                
+                # Missile fire prep
                 if shootDelay == 1:
                     if eMissileFire[i] != "fire":
                         eMX[i] = Ex[i]
@@ -452,6 +466,7 @@ def gameLoop():
                 if eMY[i] > 670:
                     eMissileFire[i] = "ready"
 
+            # When enemies reach the edge of the screen they turn around and go the other way
             if Ex[i] > display_width - 10:
                 Ey[i] = Ey[i] + 100
                 eChangeX[i] = -enemySpeed
@@ -460,25 +475,27 @@ def gameLoop():
                 Ey[i] = Ey[i] + 100
                 eChangeX[i] = enemySpeed
             
-            
+            # Missile Shooting
             if eMissileFire[i] == "fire":
-
+                # Displaying Missile
                 eFireMisslie(eMX[i],eMY[i], i)
-                
+                # Missile movement
                 eMY[i] += eMissileChange
-                # print(y)
+                
+                # Checking if player was hit
                 if eMY[i] + 50 > y and eMY[i] < y + 50:
-                    # print(eMY[i])
                     if eMX[i] < x + 90 and eMX[i] > x:
+                        # Deducting health hit sounds ect
                         health -= 1
                         hitSound.play()
                         eMissileFire[i] = "ready"
-                        largeBoom(eMX[i],eMY[i])
+                        largeBoom(eMX[i]- 20,eMY[i])
                         
                         print("hit")
 
 
-            if health == 0 or Ey[i] > y and Ex[i] < x:
+            # When the player dies all the gameplay variables are reset to default and game over screen is switched to 
+            if health == 0 or Ey[i] > y and Ex[i] < x + 50:
                 largeBoom(x,y)
                 save()
                 hitList.clear()
@@ -507,7 +524,7 @@ def gameLoop():
                 
                 gameOverScreen()
 
-
+        # If a level is completed then enemies are reset more are added and their speed is increased
         if len(hitList) == numEnemies:
             levelCounter += 1
             save()
@@ -533,51 +550,56 @@ def gameLoop():
             gameLoop()
 
 
-
+        # Respawning boss once it runs off screen
         if BossX < -100:
             BossX = 2000
-
         
-
-        
-        
-        
+        # Player Movement Updates
         x += x_change
+        # Boss Movement Updates
         BossX += bossChange
+
+        # Displaying Player on given corordinance every frame
         player(x,y)
+        # Displaying Boss on given corordinance every frame
         bossEn(BossX, BossY)
 
+        # Displaying Text every frame
         showText('Level: ' + str(levelCounter), 50, (500,0), white)
         showText('Lives: ' + str(health), 50, (700,0), white)
         showText('Score: ' + str(Score), 50, (10,0), white)
 
+        # Updating screen with tick speed limiter (Clock tick)
         pygame.display.update()
         clock.tick(100)
 
 
 
+# Game Over Screen
 def gameOverScreen():
     stop = False
     
+    # Displaying Background Image
     screen.blit(background_image, [0, 0])
 
+    # Displaying text
     showText('Game Over', 50, (100 ,100), white)
     showText('Click To Play Again', 30, (100 ,150), white)
 
     
-
+    # Checking for mouse button down and pygame event loop
     while not stop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Intro()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 Intro()
-
+        # Updating screen with tick speed limiter (Clock tick)
         pygame.display.update()
         clock.tick(120)
-
+# Running Intro
 Intro()
-
-# gameOverScreen()
+# Quiting pygame
 pygame.quit()
+# Quiting Python
 quit()
